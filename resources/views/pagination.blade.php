87da0b4cd7531930ccb68b49a845e9da4a19d515
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2023-12-17 22:16:53 +0700

    feat: custom pagination

---
+++ resources/views/pagination.blade.php
@@ -0,0 +1,25 @@
+<!-- pagination.blade.php -->
+
+@if ($paginator->lastPage() > 1)
+<ul class="pagination">
+   @if ($paginator->currentPage() > 1)
+   <li><a href="{{ $paginator->url(1) }}&perPage={{ $perPage }}">First</a></li>
+   <li><a href="{{ $paginator->url($paginator->currentPage() - 1) }}&perPage={{ $perPage }}">Previous</a></li>
+   @endif
+
+   @for ($i = 1; $i <= $paginator->lastPage(); $i++)
+      @if ($i >= $paginator->currentPage() - 2 && $i <= $paginator->currentPage() + 2)
+         <li class="{{ ($i == $paginator->currentPage()) ? 'active' : '' }}">
+            <a href="{{ $paginator->url($i) }}&perPage={{ $perPage }}">{{ $i }}</a>
+         </li>
+         @elseif (($i == $paginator->currentPage() - 3 && $i > 1) || ($i == $paginator->currentPage() + 3 && $i < $paginator->lastPage()))
+            <li><span>...</span></li>
+            @endif
+            @endfor
+
+            @if ($paginator->currentPage() < $paginator->lastPage())
+               <li><a href="{{ $paginator->url($paginator->currentPage() + 1) }}&perPage={{ $perPage }}">Next</a></li>
+               <li><a href="{{ $paginator->url($paginator->lastPage()) }}&perPage={{ $perPage }}">Last</a></li>
+               @endif
+</ul>
+@endif
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -46,17 +46,52 @@
    </tr>
    @endforeach
 </table>
-
+<p>Page {{ $products->currentPage() }} of {{ $products->lastPage() }}.</p>
 <form action="{{ route('products.index') }}" method="get">
    <label for="perPage">Items per page:</label>
    <select name="perPage" id="perPage" onchange="this.form.submit()">
-      <option value="5" {{ $perPage == 5 ? 'selected' : '' }}>5</option>
+      <option value="2" {{ $perPage == 2 ? 'selected' : '' }}>2</option>
       <option value="10" {{ $perPage == 10 ? 'selected' : '' }}>10</option>
       <option value="25" {{ $perPage == 25 ? 'selected' : '' }}>25</option>
    </select>
 </form>
 
+@include('pagination', ['paginator' => $products, 'perPage' => $perPage])
+<!-- <div class="custom-pagination">
+   <span>Page {{ $products->currentPage() }} of {{ $products->lastPage() }}.</span>
+   <span>Show per page:</span>
+
+   <a href="{{ $products->url(1) }}&perPage=5" class="{{ $perPage == 5 ? 'active' : '' }}">5</a>
+   <a href="{{ $products->url(1) }}&perPage=10" class="{{ $perPage == 10 ? 'active' : '' }}">10</a>
+   <a href="{{ $products->url(1) }}&perPage=25" class="{{ $perPage == 25 ? 'active' : '' }}">25</a>
+
+   <span>Total items: {{ $products->total() }}</span>
+
+   @if($products->lastPage() > 1)
+   <ul class="pagination">
+      @if($products->currentPage() > 1)
+      <li><a href="{{ $products->url($products->currentPage() - 1) }}&perPage={{ $perPage }}">Previous</a></li>
+      @endif
+
+      @for($i = 1; $i <= $products->lastPage(); $i++)
+         @if($i >= $products->currentPage() - 2 && $i <= $products->currentPage() + 2)
+            <li class="{{ ($i == $products->currentPage()) ? 'active' : '' }}">
+               <a href="{{ $products->url($i) }}&perPage={{ $perPage }}">{{ $i }}</a>
+            </li>
+            @elseif(($i == $products->currentPage() - 3 && $i > 1) || ($i == $products->currentPage() + 3 && $i < $products->lastPage()))
+               <li><span>...</span></li>
+               @endif
+               @endfor
+
+               @if($products->currentPage() < $products->lastPage())
+                  <li><a href="{{ $products->url($products->currentPage() + 1) }}&perPage={{ $perPage }}">Next</a></li>
+                  @endif
+   </ul>
+   @endif
+</div> -->
 <!-- {!! $products->links() !!} -->
-{!! $products->appends(['perPage' => $perPage])->links() !!}
+<!-- {!! $products->appends(['perPage' => $perPage])->links() !!} -->
+<!-- Custom pagination links -->
+
 
 @endsection
